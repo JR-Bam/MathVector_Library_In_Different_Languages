4a83af9315f1bcaec48ce345a7708f8bf2401fbc
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2024-10-20 23:04:20 +0800

    finished haskell

---
+++ haskell/app/Main.hs
@@ -0,0 +1,47 @@
+module Main where
+
+
+import Vector (vecFromList, normalize, magnitude, dot, axpy)
+
+main :: IO ()
+main = do
+    let vec1 = vecFromList [23.0, 12.0, 5.0, 4.0, 5.0]
+    let vec2 = vecFromList [-2.0, 4.0, 50.0, 6.0, -34.5]
+
+    putStr "Vec1: "
+    print vec1
+    putStr "Vec2: "
+    print vec2
+
+    putStr "Dot Product: "
+    print (dot vec1 vec2)
+
+    putStr "AXPY with alpha = -5: "
+    print (axpy (-5.0) vec1 vec2)
+
+    putStr "Vec1 + Vec2 = "
+    print (vec1 + vec2)
+
+    putStr "Vec1 - Vec2 = "
+    print (vec1 - vec2)
+
+    putStr "Vec1 == Vec2 -> "
+    print (vec1 == vec2)
+
+    let vec5 = vecFromList [-243.0, 343.0, 1.0, 54.0, 22.0, -4.0]
+    putStr "Initial Vec5: "
+    print vec5
+
+    putStr "Magnitude of Vec5: "
+    print (magnitude vec5)
+
+    let normalVec5 = normalize vec5
+    putStr "Normalized Vec5: "
+    print normalVec5
+    putStr "Magnitude of Vec5: "
+    print (magnitude normalVec5)
+
+
+
+
+        
---
+++ haskell/app/Vector.hs
@@ -0,0 +1,44 @@
+module Vector where
+
+data Vector = Vector {
+    size :: Int,
+    components :: [Float]
+} deriving (Eq)
+
+instance Show Vector where
+    show (Vector _ xs) = show xs
+
+instance Num Vector where
+    v1 + v2 | sameSize v1 v2 = Vector (size v1) (zipWith (+) (components v1) (components v2))
+            | otherwise      = error "Errors must be same size"
+    
+    v1 - v2 | sameSize v1 v2 = Vector (size v1) (zipWith (-) (components v1) (components v2))
+            | otherwise      = error "Errors must be same size"
+
+vecFromList :: [Float] -> Vector
+vecFromList [] = error "List must be nonempty."
+vecFromList xs = Vector (length xs) xs
+
+scaleVec :: Vector -> Float -> Vector
+scaleVec (Vector n xs) alpha = Vector n (map (alpha *) xs)
+
+magnitude :: Vector -> Float 
+magnitude v1 = sqrt (dot v1 v1)
+
+normalize :: Vector -> Vector
+normalize v1 = scaleVec v1 (1.0 / magnitude v1)
+
+
+
+sameSize :: Vector -> Vector -> Bool
+sameSize (Vector s1 _) (Vector s2 _) = s1 == s2
+
+dot :: Vector -> Vector -> Float
+dot v1 v2 
+    | sameSize v1 v2 = sum $ zipWith (*) (components v1) (components v2)
+    | otherwise      = error "Errors must be same size"
+
+axpy :: Float -> Vector -> Vector -> Vector
+axpy alpha v1 v2 
+    | sameSize v1 v2 = Vector (size v1) (zipWith (\x y -> x * alpha + y) (components v1) (components v2))
+    | otherwise      = error "Errors must be same size"
